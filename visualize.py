1cda79101df76d4916bddf7acc36ed98fa29c31a
Jon Staples <jstaples@nextgxdx.com>
2014-12-07 23:02:29 -0600

    Adding some of the basic visualizations I have from the iPython notebook.

--- visualize.py
+++ visualize.py
@@ -1,5 +1,26 @@
-# from vincent import AxisProperties, PropertySet, ValueRef
+import vincent
+from vincent import AxisProperties, PropertySet, ValueRef
 import pandas as pd
 
-data = pd.read_excel("bin/input/data.xls", sheet="Detail")
-ethnic = data.groupby(["Description", "Gender"])
+DATA = pd.read_excel("bin/input/data.xls", sheet="Detail")
+
+# Description is the ethnicity according to this dataset
+ETHNICITY = data.groupby(["Description"])
+
+ETHNICITY_BY_SALARY = vincent\
+    .Bar(
+        ETHNICITY
+        .mean()
+        .sort(
+            "Annual Salary",
+            ascending=False
+        )['Annual Salary'])
+
+ETHNICITY_BY_SALARY\
+    .axes[0]\
+    .properties = AxisProperties(
+         labels=PropertySet(
+             angle=ValueRef(value=45),
+             align=ValueRef(value="left")
+                )
+        )
