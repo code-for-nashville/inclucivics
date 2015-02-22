c7381c95b83f1911f683f55db402bba104986eed
Jon Staples <jstaples@nextgxdx.com>
2015-02-22 12:22:05 -0600

    Updates: Updated setup.py file to handle requirements.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='inclucivics',
-    version='0.0.1',
+    name='Inclucivics',
+    version='0.1.2',
     packages=['ntp', 'ntp.app', 'ntp.app.include', 'ntp.data', 'ntp.data.include', 'ntp.data.include.sanitize',
               'ntp.data.include.aggregate', 'ntp.data.include.rethinkdb', 'ntp.project', 'ntp.project.common',
               'ntp.project.cherrypy', 'ntp.project.rethinkdb', 'ntp.project.elasticsearch'],
+    install_requires=['rethinkdb', 'flask', 'cherrypy', 'paste'],
     url='https://github.com/code-for-nashville/hrc-employment-diversity-report',
     license='',
     author='neolytics',
     author_email='jstaples@nextgxdx.com',
-    description=''
+    description='HR demographics data visualization app for the Nashville Human Relations Commission'
 )
