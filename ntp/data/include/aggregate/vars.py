eb75db2689611d2969c8ea2f5bcd8bae1bedf811
Jon Staples <jstaples@nextgxdx.com>
2015-02-22 19:34:23 -0600

    Updates: Updating salary distributions

--- ntp/data/include/aggregate/vars.py
+++ ntp/data/include/aggregate/vars.py
@@ -1,58 +1,58 @@
 INCOME_KEY = "income_level"
 #
-# INCOME_DISTRIBUTIONS = {'Lower Income Range (Less than $33,000)': dict(
+# INCOME_DISTRIBUTIONS = {'Lower Income Range (Less than $33,000': dict(
 #     lower=None,
 #     upper=33000
-# ), 'Middle Income Range ($33,000 and $66,000)': dict(
+# ), 'Middle Income Range ($33,000 and $66,000': dict(
 #     lower=33001,
 #     upper=66000
-# ), 'Upper Income Range (Greater than $66,000)': dict(
+# ), 'Upper Income Range (Greater than $66,000': dict(
 #     lower=66001,
 #     upper=None
 # )}
 
 
 INCOME_DISTRIBUTIONS = {
-    'A. $10,000.00 and below': dict(
+    '$10,000 and below': dict(
         lower=None,
         upper=10000
-), 'B. $10,000 and $20,000)': dict(
+), '$10,000 and $20,000': dict(
     lower=10001,
     upper=20000
-), 'C. $20,000 and $30,000)': dict(
+), '$20,000 and $30,000': dict(
     lower=20000,
     upper=30000
-), 'D. $30,000 and $40,000)': dict(
+), '$30,000 and $40,000': dict(
     lower=30000,
     upper=40000
-), 'E. $40,000 and $50,000)': dict(
+), '$40,000 and $50,000': dict(
     lower=40000,
     upper=50000
-), 'F. $50,000 and $60,000)': dict(
+), '$50,000 and $60,000': dict(
     lower=50000,
     upper=60000
-), 'G. $60,000 and $70,000)': dict(
+), '$60,000 and $70,000': dict(
     lower=60000,
     upper=70000
-), 'H. $70,000 and $80,000)': dict(
+), '$70,000 and $80,000': dict(
     lower=70000,
     upper=80000
-), 'H. $80,000 and $00,000)': dict(
+), '$80,000 and $00,000': dict(
     lower=80000,
     upper=90000
-), 'I. $90,000 and $100,000)': dict(
+), '$90,000 and $100,000': dict(
     lower=90000,
     upper=100000
-), 'J. $100,000 and $110,000)': dict(
+), '$100,000 and $110,000': dict(
     lower=100000,
     upper=110000
-), 'K. $110,000 and $120,000)': dict(
+), '$110,000 and $120,000': dict(
     lower=110000,
     upper=120000
-), 'L. 12. $120,000 and $130,000)': dict(
+), '$120,000 and $130,000': dict(
     lower=120000,
     upper=130000
-), 'M. $130,000 and above)': dict(
+), '$130,000 and above': dict(
     lower=130000,
     upper=None
 
