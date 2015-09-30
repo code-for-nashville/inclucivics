7e8348d397f2245705fd5a7e18ca6f160588265d
Jon Staples <jstaples@nextgxdx.com>
2015-09-30 16:45:10 -0700

    readme: Incrementing patch
    v3.1.4
    
    No issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Inclucivics',
-    version='3.1.3',
+    version='3.1.4',
     packages=['ntp', 'ntp.app', 'ntp.app.include', 'ntp.data', 'ntp.data.include', 'ntp.data.include.sanitize',
               'ntp.data.include.aggregate', 'ntp.data.include.rethinkdb', 'ntp.project', 'ntp.project.common',
               'ntp.project.cherrypy', 'ntp.project.rethinkdb', 'ntp.project.elasticsearch'],
