7afa30a0818edccd588a87d41b290854d233ee5f
Jon Staples <jstaples@nextgxdx.com>
2015-11-16 19:41:11 -0600

    chore: Deleted almost all of the backend crap logic
    Too separated, cumbersome, too many subpackages and imports.  The
    majority of that code was useless anyway.  The app will not function in
    the current state, but sometimes you just have to wipe the slate clean.
    
    Issue #83

--- ntp/data/celeryconfig.py
+++ ntp/data/celeryconfig.py
@@ -5,7 +5,7 @@ BROKER_URL="amqp://guest@localhost//"
 CELERY_IMPORTS = ("ntp.data",)
 CELERYBEAT_SCHEDULE = {
         'request-hr-data': {
-            "task": "ntp.data.tasks",
+            "task": "ntp.data.tasks.get_last_update",
             "schedule": crontab(0, 0, day_of_week="sat")
             }   
     }
