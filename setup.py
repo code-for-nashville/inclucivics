a41b093bc1264839b149599339c5ac435f00d9db
Jon Staples <jstaples@nextgxdx.com>
2016-02-02 19:21:16 -0600

    refactor: Redefined package installation in setup.py
    Dramatic simplification of backend is now reflected in the setup.
    
    None

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup
 setup(
     name='Inclucivics',
     version='3.1.4',
-    packages=['ntp', 'ntp.app', 'ntp.app.include', 'ntp.data', 'ntp.data.include', 'ntp.data.include.sanitize',
-              'ntp.data.include.aggregate', 'ntp.data.include.rethinkdb', 'ntp.project', 'ntp.project.common',
-              'ntp.project.cherrypy', 'ntp.project.rethinkdb', 'ntp.project.elasticsearch'],
+    packages=[
+        'ntp', 
+        'ntp.app', 
+        'ntp.data' 
+    ],
     install_requires=['rethinkdb', 'flask', 'cherrypy', 'paste', 
         "celery"],
     url='https://github.com/code-for-nashville/hrc-employment-diversity-report',
