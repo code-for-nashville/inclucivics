0c54f1c8cfb63cd9db24a0f28947312a258bd970
Max Shenfield <shenfieldmax@gmail.com>
2016-03-29 23:08:33 -0500

    Use find_packages so that all packages are importable from the command line

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Inclucivics',
     version='3.1.4',
-    packages=[
-        'ntp', 
-        'ntp.app', 
-        'ntp.data' 
-    ],
-    install_requires=['rethinkdb', 'flask', 'cherrypy', 'paste', "celery", "requests", "nose", "coverage", "toolz"], 
+    packages=find_packages(),
+    install_requires=['rethinkdb', 'flask', 'cherrypy', 'paste', "celery", "requests", "nose", "coverage", "toolz"],
     url='https://github.com/code-for-nashville/hrc-employment-diversity-report',
     license='',
     author='neolytics',
