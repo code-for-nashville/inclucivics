1136cce84fe415c43a246a90815c821e5828f726
Max Shenfield <shenfieldmax@gmail.com>
2016-03-29 23:11:05 -0500

    Call the app inclucivics throughout

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Inclucivics',
-    version='3.1.4',
+    name='inclucivics',
+    version='3.2.0',
     packages=find_packages(),
     install_requires=['rethinkdb', 'flask', 'cherrypy', 'paste', "celery", "requests", "nose", "coverage", "toolz"],
     url='https://github.com/code-for-nashville/hrc-employment-diversity-report',
-    license='',
+    license='MIT',
     author='neolytics',
     author_email='jstaples@nextgxdx.com',
     description='HR demographics data visualization app for the Nashville Human Relations Commission'
