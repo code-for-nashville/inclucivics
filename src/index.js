e127e86a528e36ab052611d659f54b8cb1399c93
Max Shenfield <shenfieldmax@gmail.com>
2017-05-19 21:54:55 -0500

    Removed service worker because GH pages w/ custom domains doesn't support HTTPS
    GH pages w/ custom domains don't support HTTPS, which are required for service workers to work. This removes all service worker stuff - no point in complicating things if we aren't able to use it.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker.js'
 
 import './index.css'
 
@@ -9,5 +8,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-
-registerServiceWorker()
