207901ac20287b5a67e5d81f52ae36582fc316e9
Tim Eccleston <teccleston@axialhealthcare.com>
2017-08-01 19:21:24 -0500

    add error page

--- s3.tf
+++ s3.tf
@@ -32,6 +32,22 @@ resource "aws_s3_bucket_object" "home_page" {
   }
 }
 
+resource "aws_s3_bucket_object" "error_page" {
+  bucket = "${aws_s3_bucket.website.bucket}"
+  key    = "error.html"
+  source = "error.html"
+  acl    = "public-read"
+  etag   = "${md5(file("error.html"))}"
+  content_type = "text/html"
+  storage_class = "REDUCED_REDUNDANCY"
+
+
+  tags = {
+    terraform_user = "${data.aws_caller_identity.current.user_id}"
+    terraform = true
+  }
+}
+
 resource "aws_s3_bucket_object" "architecture_image" {
   bucket = "${aws_s3_bucket.website.bucket}"
   key    = "docs/architecture.dot.png"
