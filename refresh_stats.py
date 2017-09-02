08dc47ecf6c63e446f3b1af0c01482419160061d
Tim Eccleston <teccleston@axialhealthcare.com>
2017-09-02 18:26:24 -0500

    fix lambda so it updates file (not triggered, and not displaying)

--- refresh_stats.py
+++ refresh_stats.py
@@ -4,11 +4,11 @@ from datetime import datetime, timezone
 import boto3
 
 s3 = boto3.resource('s3')
-s3_object = s3.object(os.environ('S3_BUCKET'), os.environ('S3_KEY'))
+s3_object = s3.Object(os.environ['S3_BUCKET'], os.environ['S3_KEY'])
 
 def last_modified(event, context):
     """
     Initializes of overwrites s3 file with current time as 'last modified' text
     """
-    body = b'Last Modified @ ' +  datetime.now(timezone.utc).isoformat()
+    body = 'Last Modified @ ' +  datetime.now(timezone.utc).isoformat()
     return s3_object.put(Body=body)
