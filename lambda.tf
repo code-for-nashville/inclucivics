5bc8a4cad6ed81bc888312a1b38d2ceb303d2008
Tim Eccleston <teccleston@axialhealthcare.com>
2017-09-02 08:40:05 -0500

    :warning: Broken (gets error page) create lambda function to refresh stats upon update

---
+++ lambda.tf
@@ -0,0 +1,32 @@
+# define Archives to autozip the lambda source code
+# from https://github.com/hashicorp/terraform/issues/8344#issuecomment-265548941
+
+data "archive_file" "refresh_stats_zip" {
+    type        = "zip"
+    source_file = "refresh_stats.py"
+    output_path = "refresh_stats.zip"
+}
+
+# Don't want to give developers permissions to create / destroy roles
+# Currently finds the role by name in an existing account
+# It would be better to read it directly from a terraform project
+# Or, when necessary a different person is required to apply security changes
+data "aws_iam_role" "lambda" {
+  role_name = "lambda_role"
+}
+
+resource "aws_lambda_function" "refresh_stats"{
+  filename = "refresh_stats.zip"
+  function_name    = "refresh_stats"
+  role             = "${data.aws_iam_role.lambda.arn}"
+  handler          = "refresh_stats.last_modified"
+  source_code_hash = "${data.archive_file.refresh_stats_zip.output_base64sha256}"
+  runtime          = "python3.6"
+
+  environment {
+    variables = {
+      S3_BUCKET = "${aws_s3_bucket.website.bucket}"
+      S3_KEY = "last_modified.txt"
+    }
+  }
+}
