955246610da10fa57548131fc0cc9240ec5897f2
Max Shenfield <shenfieldmax@gmail.com>
2018-03-18 18:06:18 -0500

    Read and write to S3 using scheduled AWS Lambda (#149)
    * Issue 136: Read and write to S3 using AWS Lambda
    
    This updates our architecture to the following:
    
    AWS Lambda - Once a day fetch the latest employees CSV, transform it, and write the results to S3
    Github Pages - Static React app reads all data from S3
    
    It uses terraform with committed [local state](https://www.terraform.io/docs/backends/types/local.html) to declare the infrastructure.  This is a convenient way to have a single infrastructure instance that anyone in the project can see.  All local data has been removed, including the input/ and public/data/ folders.  Data is now stored in the public S3 bucket https://s3.amazonaws.com/codefornashville-inclucivics-c9b520.
    
    For now, the S3 bucket is hardcoded into both the terraform build, documentation, and in the React app in constants.js.  A future enhancement would be to populate this dynamically from the terraform state.  In addition, I'm the only user able to write out lambdas - we need to figure out an appropriate role that enables users to modify existing state, and create new state, up to a limit.

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# data bucket lambda writes to
+resource "aws_s3_bucket" "data" {
+  acl = "public-read"
+  # The bucket name is hardcoded into the app
+  # Make sure to change there if you change me!
+  bucket = "codefornashville-inclucivics-c9b520"
+
+  cors_rule {
+    allowed_methods =["GET"]
+    allowed_origins = [
+      "http://www.codefornashville.org/",
+      "http://localhost:3000/",
+    ]
+  }
+
+  versioning {
+    enabled = true
+  }
+}
+
+# handler archive
+data "archive_file" "ingest" {
+    type        = "zip"
+    source_dir  = "ingest"
+    output_path = "ingest.zip"
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid = "1"
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "${aws_s3_bucket.data.arn}",
+      "${aws_s3_bucket.data.arn}/*",
+    ]
+  }
+}
+
+module "scheduled_ingest" {
+  source              = "github.com/terraform-community-modules/tf_aws_lambda_scheduled"
+  lambda_name         = "inclucivics_ingest"
+  runtime             = "nodejs6.10"
+  lambda_zipfile      = "ingest.zip"
+  source_code_hash    = "${data.archive_file.ingest.output_base64sha256}"
+  handler             = "index.handler"
+  schedule_expression = "rate(1 day)"
+  timeout             = 200
+  iam_policy_document = "${data.aws_iam_policy_document.s3.json}"
+}
